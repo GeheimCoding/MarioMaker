f2a6158d13c79ecdc30e2b20238d10a59fdfee32
GeheimCoding <geheim.coding@gmail.com>
2023-09-24 12:52:23 +0200

    Add sprite sheet

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,39 @@
 use bevy::prelude::*;
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(ClearColor(Color::CYAN))
+        .add_plugins(
+            DefaultPlugins
+                .set(WindowPlugin {
+                    primary_window: Some(Window {
+                        title: "Mario Maker".to_owned(),
+                        ..default()
+                    }),
+                    ..default()
+                })
+                .set(ImagePlugin::default_nearest()),
+        )
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlases: ResMut<Assets<TextureAtlas>>,
+) {
+    // TODO: fix sprite sheet and coordinates
+    let texture_handle = asset_server.load("smw_mario_sheet.png");
+    let texture_atlas =
+        TextureAtlas::from_grid(texture_handle, Vec2::new(40.0, 38.0), 10, 1, None, None);
+    let texture_atlas_handle = texture_atlases.add(texture_atlas);
+
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(SpriteSheetBundle {
+        texture_atlas: texture_atlas_handle,
+        sprite: TextureAtlasSprite::new(4),
+        transform: Transform::from_scale(Vec3::splat(8.0)),
+        ..default()
+    });
 }
