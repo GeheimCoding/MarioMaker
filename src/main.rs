7a976655317a83fd7d0fce9e2b06e806ca5c8fa1
GeheimCoding <geheim.coding@gmail.com>
2023-09-24 18:27:19 +0200

    Add animation

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,23 @@ fn main() {
                 .set(ImagePlugin::default_nearest()),
         )
         .add_systems(Startup, setup)
+        .add_systems(Update, animate_sprite)
         .run();
+}
+
+#[derive(Component, Deref, DerefMut)]
+struct AnimationTimer(Timer);
+
+fn animate_sprite(
+    time: Res<Time>,
+    mut query: Query<(&mut AnimationTimer, &mut TextureAtlasSprite)>,
+) {
+    for (mut timer, mut sprite) in &mut query {
+        timer.tick(time.delta());
+        if timer.just_finished() {
+            sprite.index = (sprite.index + 1) % 2;
+        }
+    }
 }
 
 fn setup(
@@ -23,17 +39,19 @@ fn setup(
     asset_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
 ) {
-    // TODO: fix sprite sheet and coordinates
-    let texture_handle = asset_server.load("smw_mario_sheet.png");
+    let texture_handle = asset_server.load("textures/mario.png");
     let texture_atlas =
-        TextureAtlas::from_grid(texture_handle, Vec2::new(40.0, 38.0), 10, 1, None, None);
+        TextureAtlas::from_grid(texture_handle, Vec2::new(16.0, 24.0), 2, 1, None, None);
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     commands.spawn(Camera2dBundle::default());
-    commands.spawn(SpriteSheetBundle {
-        texture_atlas: texture_atlas_handle,
-        sprite: TextureAtlasSprite::new(4),
-        transform: Transform::from_scale(Vec3::splat(8.0)),
-        ..default()
-    });
+    commands.spawn((
+        SpriteSheetBundle {
+            texture_atlas: texture_atlas_handle,
+            sprite: TextureAtlasSprite::new(0),
+            transform: Transform::from_scale(Vec3::splat(8.0)),
+            ..default()
+        },
+        AnimationTimer(Timer::from_seconds(0.2, TimerMode::Repeating)),
+    ));
 }
