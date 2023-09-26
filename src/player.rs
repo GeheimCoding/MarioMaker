69a631e5ae2cd647a63b2fa7d762b752d3aa6284
GeheimCoding <geheim.coding@gmail.com>
2023-09-26 20:24:43 +0200

    Add horizontal player movement
    - add error handler

--- src/player.rs
+++ src/player.rs
@@ -1,14 +1,33 @@
+use bevy::ecs::query::QuerySingleError;
 use bevy::prelude::*;
+use std::fmt::Debug;
 
 pub struct PlayerPlugin;
+
+#[derive(PartialEq)]
+enum Direction {
+    Left,
+    Right,
+}
+
+#[derive(Component)]
+struct Player {
+    speed: f32,
+    direction: Direction,
+}
 
 #[derive(Component, Deref, DerefMut)]
 struct AnimationTimer(Timer);
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_player)
-            .add_systems(Update, animate_sprite);
+        app.add_systems(Startup, setup_player).add_systems(
+            Update,
+            (
+                horizontal_player_movement.pipe(error_handler),
+                animate_player.pipe(error_handler),
+            ),
+        );
     }
 }
 
@@ -23,6 +42,10 @@ fn setup_player(
     let texture_atlas_handle = texture_atlases.add(texture_atlas);
 
     commands.spawn((
+        Player {
+            speed: 80.0,
+            direction: Direction::Right,
+        },
         SpriteSheetBundle {
             texture_atlas: texture_atlas_handle,
             ..default()
@@ -31,14 +54,52 @@ fn setup_player(
     ));
 }
 
-fn animate_sprite(
+fn horizontal_player_movement(
     time: Res<Time>,
-    mut query: Query<(&mut AnimationTimer, &mut TextureAtlasSprite)>,
-) {
-    for (mut timer, mut sprite) in &mut query {
-        timer.tick(time.delta());
-        if timer.just_finished() {
-            sprite.index = (sprite.index + 1) % 2;
-        }
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&mut Transform, &mut Player)>,
+) -> Result<(), QuerySingleError> {
+    let (mut transform, mut player) = query.get_single_mut()?;
+
+    let direction = get_direction(keyboard_input);
+    if direction.x < 0.0 {
+        player.direction = Direction::Left;
+    } else if direction.x > 0.0 {
+        player.direction = Direction::Right;
+    }
+    transform.translation += direction * player.speed * time.delta_seconds();
+
+    Ok(())
+}
+
+fn get_direction(keyboard_input: Res<Input<KeyCode>>) -> Vec3 {
+    let mut direction = Vec2::ZERO;
+    if keyboard_input.any_pressed(vec![KeyCode::Left, KeyCode::A]) {
+        direction.x -= 1.0;
+    }
+    if keyboard_input.any_pressed(vec![KeyCode::Right, KeyCode::D]) {
+        direction.x += 1.0;
+    }
+    Vec3::new(direction.x, direction.y, 0.0)
+}
+
+fn animate_player(
+    time: Res<Time>,
+    mut query: Query<(&mut AnimationTimer, &mut TextureAtlasSprite, &Player)>,
+) -> Result<(), QuerySingleError> {
+    let (mut timer, mut sprite, player) = query.get_single_mut()?;
+
+    timer.tick(time.delta());
+    if timer.just_finished() {
+        sprite.index = (sprite.index + 1) % 2;
+    }
+    sprite.flip_x = player.direction == Direction::Left;
+
+    Ok(())
+}
+
+fn error_handler<E: Debug>(In(result): In<Result<(), E>>) {
+    if let Err(err) = result {
+        println!("encountered an error {:?}", err);
     }
 }
